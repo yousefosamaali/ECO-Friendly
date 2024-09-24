0502ce68bdc820165c2fdc9be94de18c68cbd33a
yousefosamaali <85324016+yousefosamaali@users.noreply.github.com>
2024-09-24 16:59:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/products", key: "products", label: "Products" },
+  { href: "/contact-us", key: "contact", label: "Contact" },
+  { href: "/about", key: "about", label: "About" },
+];
+
+//FEATURED PRODUCTS
+export const PRODUCTS = [
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Electronics",
+    key: "_id1",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Cloth",
+    key: "_id2",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Cars",
+    key: "_id3",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Food",
+    key: "_id4",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Food",
+    key: "_id5",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Food",
+    key: "_id6",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Food",
+    key: "_id7",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Food",
+    key: "_id8",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Cars",
+    key: "_id9",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Cloth",
+    key: "_id10",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Cloth",
+    key: "_id11",
+  },
+  {
+    img: "https://dummyimage.com/420x260",
+    label: "Product Name",
+    price: "Product Price",
+    category: "Electronics",
+    key: "_id12",
+  },
+];
+
+export const TEAM = [
+  {
+    img: "/team_x.jpg",
+    name: "Morgan Smith",
+    jop: "CEO, Founder",
+    key: "x",
+  },
+  {
+    img: "/team_x.jpg",
+    name: "Morgan Smith",
+    jop: "CEO, Founder",
+    key: "xx",
+  },
+  {
+    img: "/team_x.jpg",
+    name: "Morgan Smith",
+    jop: "CEO, Founder",
+    key: "xz",
+  },
+  {
+    img: "/team_x.jpg",
+    name: "Morgan Smith",
+    jop: "CEO ,Founder",
+    key: "xc",
+  },
+];
